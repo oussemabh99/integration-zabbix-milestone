7885dd448290a5c59d6e9e4483e97d0718460bbc
oussemabh99 <115352181+oussemabh99@users.noreply.github.com>
2024-05-08 15:10:22 +0100

    v1

---
+++ folder1/data_base.py
@@ -0,0 +1,32 @@
+import mysql.connector
+
+# Establish a connection to the MySQL server
+cnx = mysql.connector.connect(
+    host='192.168.0.168',  # Replace with the actual hostname
+    user='root',  # Replace with your MySQL username
+    password='Qnb1234.',  # Replace with your MySQL password
+    database='mysql'  # Replace with the name of your MySQL database
+)
+
+# Create a cursor object to interact with the database
+cursor = cnx.cursor()
+
+# Execute a SELECT query
+query = "SELECT * FROM user"
+cursor.execute(query)
+
+# Fetch all rows returned by the query
+rows = cursor.fetchall()
+
+# Process the rows
+for row in rows:
+    # Access individual columns using row[column_index]
+    
+    column1_value = row[0]
+    column2_value = row[1]
+    print(str(column1_value)+" | "+str(column2_value))
+    # ...
+
+# Close the cursor and the connection
+cursor.close()
+cnx.close()
