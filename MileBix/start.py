1b0eefd4d5acfcb39326e5a0976c8ae21053d3d0
oussemabh99 <115352181+oussemabh99@users.noreply.github.com>
2024-08-05 10:01:52 +0100

    Update start.py

--- MileBix/start.py
+++ MileBix/start.py
@@ -1,7 +1,7 @@
 import subprocess
 import json
 # Se connecter au serveur de gestion avec des informations d'identification spécifiées
-connect_command =  '$User="qnbts-video\oussema";$PWord = ConvertTo-SecureString -String "Qnb1234." -AsPlainText -Force;$Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $User, $PWord;Connect-ManagementServer -server  192.168.0.168 -Credential $Credential;Get-VmsCameraReport |ConvertTo-Json'
+connect_command =  '$User="passwordts-video\oussema";$PWord = ConvertTo-SecureString -String "password1234." -AsPlainText -Force;$Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $User, $PWord;Connect-ManagementServer -server  192.168.0.168 -Credential $Credential;Get-VmsCameraReport |ConvertTo-Json'
 connect_result = subprocess.run(["powershell","-c", connect_command],capture_output=True, text=True)
 if(connect_result.stderr):
     #print(connect_result.stdout)
